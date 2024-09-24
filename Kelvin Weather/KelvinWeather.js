a4391b988439e0406d6e844b1dd729a9b2b7eac9
RayaMihaylova <raya.p.mihaylova@gmail.com>
2024-09-24 09:23:03 +0300

    init push

---
+++ Kelvin Weather/KelvinWeather.js
@@ -0,0 +1,10 @@
+// kelvin is a const and it is equal to 293;
+const kelvin = 0;
+// celsius is 273 lower than kelvin;
+let celsius = kelvin - 273;
+// fahrenheit is an equation you dont have to know it;
+//i have to write a comment
+let fahrenheit = Math.floor(celsius * (9/5) + 32);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+let newton = Math.floor(celsius * (33/100));
+console.log(newton);
